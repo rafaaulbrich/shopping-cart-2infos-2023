194511a9b3675904e644fbc2a88675cd5e6871fe
rafaaulbrich <rafaelacamilly2018@gmail.com>
2023-05-26 11:14:37 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,16 @@
 <script setup>
-import { ref } from 'vue'
 import { livros } from '@/_data/livros.js'
-
-const carrinho = ref({
-  itens: [],
-  total: 0
-})
-
-function atualizaQuantidadeItem(item) {
-  carrinho.value.total -= item.total
-  item.total = item.price * item.quantidade
-  carrinho.value.total += item.total
-}
-
-function removerItemCarrinho(item) {
-  const index = carrinho.value.itens.findIndex((i) => i.id === item.id)
-  carrinho.value.total -= item.total
-  carrinho.value.itens.splice(index, 1)
-}
-
-function adicionarAoCarrinho(livro) {
-  const index = carrinho.value.itens.findIndex((item) => item.id === livro.id)
-  if (index === -1) {
-    carrinho.value.itens.push({
-      ...livro,
-      quantidade: 1,
-      total: livro.price
-    })
-    carrinho.value.total += livro.price
-  } else {
-    carrinho.value.itens[index].quantidade++
-    carrinho.value.itens[index].total += livro.price
-    carrinho.value.total += livro.price
-  }
-}
+import {
+  carrinho,
+  adicionarAoCarrinho,
+  removerItemCarrinho,
+  atualizaQuantidadeItem
+} from '@/_data/carrinho.js'
 
 function formatarPreco(preco) {
   return 'R$ ' + preco.toFixed(2).replace('.', ',')
 }
+
 </script>
 
 <template>
